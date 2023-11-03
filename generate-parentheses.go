d2cec0e6a0296b9adca0e6a2341ffdbc12c4e893
johngeorge47 <johngeorge2049@gmail.com>
2023-11-03 15:19:29 +0530

    worked on first try

--- generate-parentheses.go
+++ generate-parentheses.go
@@ -1,1 +1,24 @@
 package dsalgo
+
+/*
+https://leetcode.com/problems/generate-parentheses/description/
+*/
+
+func generateParenthesis(n int) []string {
+	result := make([]string, 0)
+	return result
+}
+
+func backTrackParentesis(n, left, right int, currStr string, result *[]string) {
+	if len(currStr) == 2*n {
+		*result = append(*result, currStr)
+		return
+	}
+	if left < n {
+		backTrackParentesis(n, left+1, right, currStr+"(", result)
+	}
+	if right < left {
+		backTrackParentesis(n, left, right+1, currStr+")", result)
+	}
+
+}
