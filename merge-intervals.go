f05714f986c8e6c8a1790e0a597b1ef373aa4a06
johngeorge47 <johngeorge2049@gmail.com>
2023-11-16 09:07:17 +0530

    merging intervals ]

--- merge-intervals.go
+++ merge-intervals.go
@@ -1,1 +1,17 @@
 package dsalgo
+
+func merge(intervals [][]int) [][]int {
+	var result [][]int
+	currInterval := intervals[0]
+	for i := 1; i < len(intervals); i++ {
+		interval := intervals[i]
+		if currInterval[1] >= interval[0] && interval[1] >= currInterval[0] {
+			currInterval = []int{min(currInterval[0], interval[0]), max(interval[1], currInterval[1])}
+		} else {
+			result = append(result, currInterval)
+			currInterval = interval
+		}
+	}
+	result = append(result, currInterval)
+	return result
+}
